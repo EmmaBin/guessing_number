60bbd3d4b488889dd54ea557db71b79501b67698
emmacodes <binma.emma@gmail.com>
2022-08-09 16:27:46 -0700

    added play again and best score tracking

--- game.py
+++ game.py
@@ -1,35 +1,52 @@
 """A number-guessing game."""
 from random import randint
+best_score = 101
+def guessingGame():
+    secret_num = randint(1, 100)
+    print(secret_num)
+    print(f"{name}, I'm thinking of a number between 1 and 100.")
+
+    guess_count = 0
+    while True:
+        while True:
+            user_guess = input("Guess a number: ")
+            try:
+                int(user_guess)
+            except:
+                print("ERROR: Please input a number.")
+            else:
+                user_guess = int(user_guess)
+
+                if user_guess < 1 or user_guess > 100:
+                    print("ERROR: Please choose a number between 1 and 100.")
+                else:
+                    break
+        guess_count += 1
+        if user_guess == secret_num:        
+            print(f'Well done, {name} you guessed the correct number in {guess_count} tries!')
+            return guess_count
+            break
+        elif user_guess > secret_num:
+            print('Your guess is too high, try again!')  
+        else:
+            print('Your guess is too low, try again!')
+
 print('Howdy, what is your name?')
 name = input('type in your name: ')
-secret_num = randint(1, 100)
-print(f"{name}, I'm thinking of a number between 1 and 100.")
 
-guess_count = 0
-while True:
-    while True:
-        user_guess = input("Guess a number: ")
-        try:
-            int(user_guess)
-        except:
-            print("ERROR: Please input a number.")
-        else:
-            user_guess = int(user_guess)
 
-            if user_guess < 1 or user_guess > 100:
-                print("ERROR: Please choose a number between 1 and 100.")
-            else:
-                break
-    guess_count += 1
-    if user_guess == secret_num:        
-        print(f'Well done, {name} you guessed the correct number in {guess_count} tries!')
-        break
-    elif user_guess > secret_num:
-        print('Your guess is too high, try again!')  
+play_again = True
+while play_again: 
+    guess_count = guessingGame()
+    if guess_count < best_score:
+        best_score = guess_count
+        print(f'Congratulations, you have a new best score {best_score}!')
     else:
-        print('Your guess is too low, try again!')
-
-
+        print(f'You still have not beaten your best score of {best_score}, try again!')    
+    player_choice = input('Would you like to play again? y/n: ')
+    if player_choice == 'n':
+        play_again = False
+        
 
 
 # loop
